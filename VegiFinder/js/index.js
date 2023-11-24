4f80165d13862568b1a1601882f29464f3f421e3
daniel gaya vicente <danielgaya200233@gmail.com>
2023-11-24 02:17:07 +0100

    VegiFinder

--- VegiFinder/js/index.js
+++ VegiFinder/js/index.js
@@ -3,10 +3,21 @@ function searchProducts() {
 
     // Crear botones para cada página web
     const websites = [
-        { name: 'OpenFoodFacts', url: `https://es.openfoodfacts.org/cgi/search.pl?search_terms=${searchTerm}&search_simple=1&action=process` },
-        { name: 'Producto Vegano', url: `https://www.productovegano.net/?s=${searchTerm}` },
-        { name: 'Vegano por Accidente Spain', url: `https://www.veganoporaccidentespain.com/?s=${searchTerm}` },
-        { name: 'SuperVeggie', url: `https://superveggie.es/?s=${searchTerm}` }
+        {name: 'OpenFoodFacts',
+         url: `https://es.openfoodfacts.org/cgi/search.pl?search_terms=${searchTerm}&search_simple=1&action=process`,
+         image: './img/openfoodfacts.png'},
+
+        {name: 'Producto Vegano',
+         url: `https://www.productovegano.net/?s=${searchTerm}`,
+         image: './img/productovegano.png'},
+
+        {name: 'Vegano por Accidente Spain',
+         url: `https://www.veganoporaccidentespain.com/?s=${searchTerm}`,
+         image: './img/veganoporaccidente.png'},
+
+        {name: 'SuperVeggie',
+         url: `https://superveggie.es/?s=${searchTerm}`,
+         image: './img/superveggie.png'}
     ];
 
     displayResults(websites);
@@ -16,14 +27,21 @@ function displayResults(websites) {
     const resultsContainer = document.getElementById('search-results');
     resultsContainer.innerHTML = ''; // Limpiar resultados anteriores
 
+    document.getElementById("search-results").style.visibility = "visible";
+
     websites.forEach(website => {
         const button = document.createElement('button');
-        button.textContent = `Ver en ${website.name}`;
-        button.className = 'btn btn-secondary mr-2';
+        button.innerHTML = `
+            <img src="${website.image}" alt="${website.name}" class="site-image">
+            Ver en ${website.name}
+        `;
+        button.className = 'btn btn-secondary mr-2 result-button'; // Agrega la clase 'result-button'
         button.addEventListener('click', function() {
             window.open(website.url, '_blank');
+            button.classList.add('clicked'); // Agrega la clase 'clicked' al botón después de hacer clic
         });
 
         resultsContainer.appendChild(button);
     });
 }
+
