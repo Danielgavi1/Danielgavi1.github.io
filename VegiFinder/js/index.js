4569ad2680e4a32c61a7f8b839b9604df180386f
daniel gaya vicente <danielgaya200233@gmail.com>
2023-11-29 21:17:03 +0100

    VegiFinder

--- VegiFinder/js/index.js
+++ VegiFinder/js/index.js
@@ -1,23 +1,19 @@
+document.getElementById('search-button').addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        // Si la tecla presionada es Enter, ejecutar la búsqueda
+        searchProducts();
+    }
+});
+
 function searchProducts() {
-    const searchTerm = document.getElementById('search-input').value;
+    const searchTerm = document.getElementById('search-button').value;
 
     // Crear botones para cada página web
     const websites = [
-        {name: 'OpenFoodFacts',
-         url: `https://es.openfoodfacts.org/cgi/search.pl?search_terms=${searchTerm}&search_simple=1&action=process`,
-         image: './img/openfoodfacts.png'},
-
-        {name: 'Producto Vegano',
-         url: `https://www.productovegano.net/?s=${searchTerm}`,
-         image: './img/productovegano.png'},
-
-        {name: 'Vegano por Accidente Spain',
-         url: `https://www.veganoporaccidentespain.com/?s=${searchTerm}`,
-         image: './img/veganoporaccidente.png'},
-
-        {name: 'SuperVeggie',
-         url: `https://superveggie.es/?s=${searchTerm}`,
-         image: './img/superveggie.png'}
+        { name: 'OpenFoodFacts', url: `https://es.openfoodfacts.org/cgi/search.pl?search_terms=${searchTerm}&search_simple=1&action=process`, image: './img/openfoodfacts.png' },
+        { name: 'Producto Vegano', url: `https://www.productovegano.net/?s=${searchTerm}`, image: './img/productovegano.png' },
+        { name: 'Vegano por Accidente Spain', url: `https://www.veganoporaccidentespain.com/?s=${searchTerm}`, image: './img/veganoporaccidente.png' },
+        { name: 'SuperVeggie', url: `https://superveggie.es/?s=${searchTerm}`, image: './img/superveggie.png' }
     ];
 
     displayResults(websites);
@@ -33,10 +29,9 @@ function displayResults(websites) {
         const button = document.createElement('button');
         button.innerHTML = `
             <img src="${website.image}" alt="${website.name}" class="site-image">
-            Ver en ${website.name}
-        `;
+            Ver en ${website.name}`;
         button.className = 'btn btn-secondary mr-2 result-button'; // Agrega la clase 'result-button'
-        button.addEventListener('click', function() {
+        button.addEventListener('click', function () {
             window.open(website.url, '_blank');
             button.classList.add('clicked'); // Agrega la clase 'clicked' al botón después de hacer clic
         });
@@ -44,4 +39,3 @@ function displayResults(websites) {
         resultsContainer.appendChild(button);
     });
 }
-
