c466ba0c77a0e37c5b2d245ad499b581a2c32541
daniel gaya vicente <danielgaya200233@gmail.com>
2025-07-18 15:43:33 +0200

    Calculadora_tmb modo offline

---
+++ calculadora_tmb/service-worker.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'metabolicpro-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/img/MetabolicPro.png',
+  '/img/icon-192.png',
+  '/img/icon-512.png',
+  'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css'
+];
+
+// Instalación
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Activación
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(name => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
