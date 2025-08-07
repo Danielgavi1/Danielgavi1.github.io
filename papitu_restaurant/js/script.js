ba1f8d2ded947a09ddc413ee1d699b9143b19a4b
daniel gaya vicente <danielgaya200233@gmail.com>
2025-08-07 16:21:34 +0200

    Papitu Restaurant

--- papitu_restaurant/js/script.js
+++ papitu_restaurant/js/script.js
@@ -1,2 +1,20 @@
 // SEGURIDAD
 document.oncontextmenu = function(){return false;}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const mapSections = document.querySelectorAll('.map-placeholder');
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        const iframe = entry.target.querySelector('iframe');
+        if (!iframe.src) {
+          iframe.src = iframe.dataset.src; // Carga la URL del mapa desde data-src
+        }
+        observer.unobserve(entry.target);
+      }
+    });
+  }, { threshold: 0.1 });
+
+  mapSections.forEach(section => observer.observe(section));
+});
