f390e0e20534ec38dd575156cac37189782c6e57
daniel gaya vicente <danielgaya200233@gmail.com>
2025-08-29 08:53:45 +0200

    Papitu Restaurant

--- papitu_restaurant/js/script.js
+++ papitu_restaurant/js/script.js
@@ -1,5 +1,6 @@
 document.oncontextmenu=function(){return!1},document.addEventListener("DOMContentLoaded",(()=>{const e=document.querySelectorAll(".map-placeholder"),t=new IntersectionObserver(((e,t)=>{e.forEach((e=>{if(e.isIntersecting){const r=e.target.querySelector("iframe");r.src||(r.src=r.dataset.src),t.unobserve(e.target)}}))}),{threshold:0,rootMargin:"300px 0px"});e.forEach((e=>t.observe(e)))})),document.getElementById("year").textContent=(new Date).getFullYear(),document.addEventListener("DOMContentLoaded",(()=>{const e=document.querySelectorAll(".reveal-up, .reveal-left, .reveal-right"),t=new IntersectionObserver((e=>{e.forEach((e=>{e.isIntersecting&&(e.target.classList.add("show"),t.unobserve(e.target))}))}),{threshold:.1,rootMargin:"0px 0px -50px 0px"});e.forEach((e=>t.observe(e)))}));
 
+// Toggle Idiomas
 (function () {
   const toggles = document.querySelectorAll('.lang-toggle');
 
@@ -36,3 +37,57 @@ document.oncontextmenu=function(){return!1},document.addEventListener("DOMConten
     toggle.addEventListener('click', toggleMenu);
   });
 })();
+
+
+// MODAL
+function openModal() {
+  document.getElementById("modalReservar").style.display = "flex";
+}
+function closeModal() {
+  document.getElementById("modalReservar").style.display = "none";
+}
+
+// Cerrar al hacer clic fuera
+window.onclick = function(event) {
+  const modal = document.getElementById("modalReservar");
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+// VALIDACIÓN CORREO
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.querySelector("#modalReservar form");
+    const emailInput = document.getElementById("email");
+
+    form.addEventListener("submit", function(e) {
+        const email = emailInput.value.trim();
+        const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+        if (!regex.test(email)) {
+            e.preventDefault(); // Evita envío
+            alert("Por favor, introduce un correo electrónico válido.");
+            emailInput.focus();
+            return false;
+        }
+    });
+});
+
+
+
+// VALIDACIÓN FECHA
+document.addEventListener("DOMContentLoaded", () => {
+  const fechaInput = document.getElementById("fecha");
+  const ahora = new Date();
+
+  // Formatear a YYYY-MM-DDTHH:MM (lo que espera datetime-local)
+  const año = ahora.getFullYear();
+  const mes = String(ahora.getMonth() + 1).padStart(2, "0");
+  const dia = String(ahora.getDate()).padStart(2, "0");
+  const horas = String(ahora.getHours()).padStart(2, "0");
+  const minutos = String(ahora.getMinutes()).padStart(2, "0");
+
+  const fechaMin = `${año}-${mes}-${dia}T${horas}:${minutos}`;
+  fechaInput.setAttribute("min", fechaMin);
+});
